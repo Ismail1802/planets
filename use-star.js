b12ce1d6253412877bb06f2819e0600c57496df5
Ismail1802 <Ismail1802@users.noreply.github.com>
2023-06-29 15:58:14 +0000

    deploy: 4c3dba9b614b7e3f825136860b23170c76f0a0ce

--- use-star.js
+++ use-star.js
@@ -2,7 +2,6 @@ function useStar() {
   const STAR_COLOR = "#fff";
   const STAR_SIZE = 3;
   const STAR_MIN_SCALE = 0.2;
-  const OVERFLOW_THRESHOLD = 50;
   const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8;
 
   const canvas = document.querySelector("canvas");
@@ -14,21 +13,13 @@ function useStar() {
 
   let stars = [];
 
-  let pointerX, pointerY;
-
   let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
-
-  let touchInput = false;
 
   generate();
   resize();
   step();
 
   window.onresize = resize;
-  canvas.onmousemove = onMouseMove;
-  canvas.ontouchmove = onTouchMove;
-  canvas.ontouchend = onMouseLeave;
-  document.onmouseleave = onMouseLeave;
 
   function generate() {
     for (let i = 0; i < STAR_COUNT; i++) {
@@ -43,49 +34,6 @@ function useStar() {
   function placeStar(star) {
     star.x = Math.random() * width;
     star.y = Math.random() * height;
-  }
-
-  function recycleStar(star) {
-    let direction = "z";
-
-    let vx = Math.abs(velocity.x),
-      vy = Math.abs(velocity.y);
-
-    if (vx > 1 || vy > 1) {
-      let axis;
-
-      if (vx > vy) {
-        axis = Math.random() < vx / (vx + vy) ? "h" : "v";
-      } else {
-        axis = Math.random() < vy / (vx + vy) ? "v" : "h";
-      }
-
-      if (axis === "h") {
-        direction = velocity.x > 0 ? "l" : "r";
-      } else {
-        direction = velocity.y > 0 ? "t" : "b";
-      }
-    }
-
-    star.z = STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE);
-
-    if (direction === "z") {
-      star.z = 0.1;
-      star.x = Math.random() * width;
-      star.y = Math.random() * height;
-    } else if (direction === "l") {
-      star.x = -OVERFLOW_THRESHOLD;
-      star.y = height * Math.random();
-    } else if (direction === "r") {
-      star.x = width + OVERFLOW_THRESHOLD;
-      star.y = height * Math.random();
-    } else if (direction === "t") {
-      star.x = width * Math.random();
-      star.y = -OVERFLOW_THRESHOLD;
-    } else if (direction === "b") {
-      star.x = width * Math.random();
-      star.y = height + OVERFLOW_THRESHOLD;
-    }
   }
 
   function resize() {
@@ -125,14 +73,6 @@ function useStar() {
       star.z += velocity.z;
 
       // recycle when out of bounds
-      if (
-        star.x < -OVERFLOW_THRESHOLD ||
-        star.x > width + OVERFLOW_THRESHOLD ||
-        star.y < -OVERFLOW_THRESHOLD ||
-        star.y > height + OVERFLOW_THRESHOLD
-      ) {
-        recycleStar(star);
-      }
     });
   }
 
@@ -158,38 +98,6 @@ function useStar() {
 
       context.stroke();
     });
-  }
-
-  function movePointer(x, y) {
-    if (typeof pointerX === "number" && typeof pointerY === "number") {
-      let ox = x - pointerX,
-        oy = y - pointerY;
-
-      velocity.tx = velocity.tx + (ox / 8) * scale * (touchInput ? 1 : -1);
-      velocity.ty = velocity.ty + (oy / 8) * scale * (touchInput ? 1 : -1);
-    }
-
-    pointerX = x;
-    pointerY = y;
-  }
-
-  function onMouseMove(event) {
-    touchInput = false;
-
-    movePointer(event.clientX, event.clientY);
-  }
-
-  function onTouchMove(event) {
-    touchInput = true;
-
-    movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
-
-    event.preventDefault();
-  }
-
-  function onMouseLeave() {
-    pointerX = null;
-    pointerY = null;
   }
 }
 useStar();
